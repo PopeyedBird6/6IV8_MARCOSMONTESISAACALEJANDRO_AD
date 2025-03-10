450cd81a2de15164a5dcbb115796daa7fb09ccac
PopeyedBird6 <isaac45604@gmail.com>
2025-03-10 15:44:22 -0600

    Cambio aprobados

--- ElementosBasicosEstadistica/02Diccionario.py
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 def estadistica_notas(notas):
     notas = pd.Series(notas)
-    estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Minimo', 'Maximo', 'Desviacion estandar'])
+    estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index =['Minimo', 'Maximo', 'Desviacion estandar'])
     return estadisticas
 
 notas = {'Juan': 10, 'Isaac': 1, 'Fabian': 6, 'Maxi': 2, 'Owen': 3, 'Maria': 4, 'Maclovio': 1}
 
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas>= 5].sort_values(ascending=false)
+
 print(estadistica_notas(notas))
-    
-    
